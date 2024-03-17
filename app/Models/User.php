60f69d93805699019679f9c401baabd387d268a6
jackilyno1 <minhhap203@gmail.com>
2024-03-17 17:06:17 +0700

    Chức nagưn

--- app/Models/User.php
+++ app/Models/User.php
@@ -19,7 +19,7 @@ class User extends Authenticatable
     protected $fillable = [
         'name', 'email', 'password',
     ];
-
+    //Chức năng thêm
     /**
      * The attributes that should be hidden for arrays.
      *
@@ -37,4 +37,5 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+    //lalala
 }
